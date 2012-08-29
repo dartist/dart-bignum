eb6e9a071b89fc82c49a963b9feb39d3c0b8622c
adam <financeCoding@gmail.com>
2012-08-28 23:41:11 -0700

    dart runner script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DART_SDK=/Applications/dart/dart-sdk
+PATH=$PATH:$DART_SDK/bin
 PACK_DIR=`pwd`/packages/
 cmd="dart --enable_type_checks --enable_asserts --package-root=$PACK_DIR ./tests/test_big_integer_v8.dart"
 echo $cmd
