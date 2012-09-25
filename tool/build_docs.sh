855f3ed0930686bac23c6e466bfeb78a754c88ac
adam <financeCoding@gmail.com>
2012-09-24 23:53:04 -0700

    Updated doc tool location

--- tool/build_docs.sh
+++ tool/build_docs.sh
@@ -2,6 +2,6 @@
 DART_SDK=/Applications/dart/dart-sdk
 PATH=$PATH:$DART_SDK/bin
 PACK_DIR=`pwd`/packages/
-cmd="dart --heap_growth_rate=32 /Applications/dart/dart-sdk/pkg/dartdoc/dartdoc.dart bignum.dart"
+cmd="dart --heap_growth_rate=32 /Applications/dart/dart-sdk/pkg/dartdoc/bin/dartdoc.dart lib/bignum.dart"
 echo $cmd
 exec $cmd
